af7adf4bd4fd138e1c988a285856322cb7f8edc4
Mihail Ivanov <johndoe@example.com>
2024-06-03 22:12:11 +0300

    minor fix

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -3,20 +3,18 @@ package jm.task.core.jdbc.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 
 public class Util {
-    // реализуйте настройку соеденения с БД
-    private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/exampledb";
-    private static final String DB_USER = "root";
-    private static final String DB_PASSWORD = "210596Mixa";
-
     public static Connection getConnection() {
-        Connection conn = null;
-        try {
-            Class.forName(DB_DRIVER);
-            conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+        Connection conn;
+        try{
+            // реализуйте настройку соеденения с БД
+            ResourceBundle reader = ResourceBundle.getBundle("dbconfig");
+            Class.forName(reader.getString("db.driver"));
+            conn = DriverManager.getConnection(reader.getString("db.url"), reader.getString("db.username"),
+                                               reader.getString("db.password"));
             return conn;
         } catch (SQLException | ClassNotFoundException e) {
             throw new RuntimeException("Could not get connection", e);
